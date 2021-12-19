ecaf59694c501ca86e8729715f56b3f7ae9afd95
uliana fomina <6758599@gmail.com>
2021-12-19 22:15:55 +0300

    added query to API

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,37 +1,61 @@
 import React from "react";
 
+const base_Path= 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+
 class HomePage extends React.Component{
+  
   constructor(props) {
     super(props);
-    this.state = {word: ""};
+    this.state = {
+      word: "",
+      result: [],
+    };
     this.onChange = this.onChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+    fetchData = (word) => {
+      fetch(`${base_Path}${word}`)
+        .then(res => res.json())
+        .then(result => this.setHomePage(result))
+        .catch(error => error);
+    }
+
+    setHomePage = result =>{
+      this.setState({result});
     }
     
     onChange(e) {
-      var val = e.target.value;
-      this.setState({word: val});
+        var val = e.target.value;
+        this.setState({word: val});
     }
-   
+    
     handleSubmit(e) {
       e.preventDefault();
-      alert("Имя: " + this.state.word);
+      const {word}=this.state;
+      if(parseFloat(word)==word){
+        alert("is not a text");
+      }
+      else{
+        this.fetchData(word);
+        console.log(this.state.result);
+      }
     }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className="home-page">
         <div className="content">
           <h2 className="home-text">Welcome to the dictionary</h2>
           <form className="home-form" onSubmit={this.handleSubmit}>
             <div className="form-content">
-              <input className="btn-reset home-input" type="text" value={this.state.name} onChange={this.onChange}/>
+              <input className="btn-reset home-input" type="text" value={this.state.word} onChange={this.onChange}/>
               <input className="btn-reset home-submit" type="submit" value=""/>
             </div>
           </form>
         </div>
       </div>
-    )
+    );
   }
 }
 export default HomePage;
