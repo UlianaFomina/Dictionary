59ef634c9909d0c8990a20cbf9055d310ed2e0e3
uliana fomina <6758599@gmail.com>
2021-12-21 00:44:36 +0300

    add loader

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React from 'react';
+import Loader from './Loader';
+import logo from '../styles/images/loader.svg';
 
 const base_Path= 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 
@@ -9,6 +11,7 @@ class HomePage extends React.Component{
     this.state = {
       word: "",
       result: [],
+      load: false,
     };
     this.onChange = this.onChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -19,12 +22,19 @@ class HomePage extends React.Component{
         .then(res => res.json())
         .then(result => this.setHomePage(result))
         .catch(error => error);
+        this.setLoad();
     }
 
     setHomePage = result =>{
       this.setState({result});
     }
     
+    setLoad(){
+      this.setState((state)=>{
+        return{load: !state.load}
+      })
+    }
+
     onChange(e) {
         var val = e.target.value;
         this.setState({word: val});
@@ -33,28 +43,34 @@ class HomePage extends React.Component{
     handleSubmit(e) {
       e.preventDefault();
       const {word}=this.state;
+      this.setLoad();
       if(parseFloat(word)==word){
         alert("is not a text");
       }
       else{
-        this.fetchData(word);
-        console.log(this.state.result);
+        setTimeout(this.fetchData, 3000, word);
+        //this.fetchData(word);
       }
+      console.log(this.state.result);
     }
+
 
   render() {
     return (
-      <div className="home-page">
-        <div className="content">
-          <h2 className="home-text">Welcome to the dictionary</h2>
-          <form className="home-form" onSubmit={this.handleSubmit}>
-            <div className="form-content">
-              <input className="btn-reset home-input" type="text" value={this.state.word} onChange={this.onChange}/>
-              <input className="btn-reset home-submit" type="submit" value=""/>
-            </div>
-          </form>
-        </div>
-      </div>
+      <>
+        {this.state.load ? <img src={logo}/> : 
+        (<div className="home-page">
+          <div className="content">
+            <h2 className="home-text">Welcome to the dictionary</h2>
+            <form className="home-form" onSubmit={this.handleSubmit}>
+              <div className="form-content">
+                <input className="btn-reset home-input" type="text" value={this.state.word} onChange={this.onChange}/>
+                <input className="btn-reset home-submit" type="submit" value=""/>
+              </div>
+            </form>
+          </div>
+        </div>)}
+      </>
     );
   }
 }
